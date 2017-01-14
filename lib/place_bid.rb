9d70671bd2750bb494671bef81b536b6bd379494
Stephen John <sandrewjohn86@gmail.com>
2017-01-14 05:29:45 -0400

    Notify Bidders about Wins and losses

--- lib/place_bid.rb
+++ lib/place_bid.rb
@@ -1,5 +1,5 @@
 class PlaceBid
-	attr_reader :auction
+	attr_reader :auction, :status
 	
 	def initialize options
 		@value = options[:value].to_f
@@ -9,14 +9,19 @@ class PlaceBid
 
 	def execute
 		@auction = Auction.find @auction_id
-
-		if @value <= @auction.current_bid
+		
+		if auction.ended? && auction.top_bid.user_id == @user_id
+			@status = :won
 			return false
 		end
 
-		@bid = @auction.bids.build value: @value, user_id: @user_id
+		if @value <= auction.current_bid
+			return false
+		end
 
-		if @bid.save
+		bid = auction.bids.build value: @value, user_id: @user_id
+
+		if bid.save
 			return true
 		else
       		return false
--- test/lib/place_bid_test.rb
+++ test/lib/place_bid_test.rb
@@ -6,7 +6,7 @@ class PlaceBidTest < MiniTest::Test
 	    @user = User.create! email: "tutsplus@josemota.net", password: "password"
 	    @another_user = User.create! email: "another_user@josemota.net", password: "password"
 	    @product = Product.create! name: "Some Product"
-	    @auction = Auction.create! value: 10, product_id: @product.id
+	    @auction = Auction.create! value: 10, product_id: @product.id, ends_at: Time.now + 24.hours
   	end
 
 	def test_it_places_a_bid
@@ -24,11 +24,32 @@ class PlaceBidTest < MiniTest::Test
 	def test_fails_to_place_bid_under_current_value
 		service = PlaceBid.new(
 			value: 9,
-			user_id: @another_user.id,
-			auction_id: @auction.id 
+			user_id: another_user.id,
+			auction_id: auction.id 
 		)
 
 		refute service.execute, "Bid should not be placed"
+	end
+
+	def test_notifies_if_auction_has_won
+		service = PlaceBid.new(
+			value: 11,
+			user_id: user.id,
+			auction_id: auction.id 
+		)
+
+		service.execute
+
+		another_service = PlaceBid.new(
+			value: 190000,
+			user_id: user.id,
+			auction_id: auction.id 
+		)
+
+		Timecop.travel Time.now + 25.hours
+		another_service.execute
+
+		assert_equal :won, another_service.status
 	end
 
 	private
